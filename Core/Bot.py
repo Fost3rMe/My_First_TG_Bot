11eeb101bbeb9110b7e585dfc25328aaa595647f
Fost3rMe <79306860+Fost3rMe@users.noreply.github.com>
2021-03-27 01:40:19 +1000

    Created notification module

--- Core/Bot.py
+++ Core/Bot.py
@@ -1,29 +1,46 @@
 import telebot
+import requests
 
 bot = telebot.TeleBot('1643056128:AAHIS74BGDDf7ldzj5DQLCFC1ZQv4JOvVEE')
+fact_request = requests.get('https://cat-fact.herokuapp.com/facts/random')
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
+
 @bot.message_handler(commands=['commands'])
 def send_welcome(message):
     bot.reply_to(message, "Вы можете использовать следующие "
-                          "команды:{Здесь должен быть список команд}")
+                          "команды:{"
+                          "/start - Инициализация"
+                          "/commands - Вывести список доступных команд]"
+                          "/fact - получить интересный факт"
+                          "бот понимает следующие слова и предложения:"
+                          "привет, пока, как дела?, как тебя зовут?")
+
+
+@bot.message_handler(commands=['fact'])
+def get_fact(message):
+    bot.reply_to(message, (fact_request.json()['text']))
 
 
 @bot.message_handler(content_types=['text'])
 def get_send_message(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'привет')
+        bot.send_message(message.from_user.id, 'Привет!')
+    if message.text.lower() == 'пока':
+        bot.send_message(message.from_user.id, 'Пока')
+    if message.text.lower() == 'как дела?':
+        bot.send_message(message.from_user.id, 'Хорошо, спасибо!')
+    if message.text.lower() == 'как тебя зовут?':
+        bot.send_message(message.from_user.id, 'Бот. Джеймс Бот.')
     else:
-        bot.send_message(message.from_user.id, 'what are u want?')
+        bot.send_message(message.from_user.id, 'Прости, я тебя не понимаю.. '
+                                               'Воспользуйся списком доступных команд / commands')
 
 
-#@bot.message_handler(func=lambda m: True)
-#def echo_all(message):
-#    bot.reply_to(message, "i don't understand, sorry")
 
 
 bot.polling()
--- Core/Core_file.py
+++ Core/Core_file.py
@@ -1,17 +1,72 @@
 import telebot
+import requests
+import datetime
+from threading import Thread
+import time
+
+userlist =[]
+
 
 bot = telebot.TeleBot('1643056128:AAHIS74BGDDf7ldzj5DQLCFC1ZQv4JOvVEE')
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+class Fact:
+
+    def __init__(self, url='https://cat-fact.herokuapp.com/facts/random'):
+        self.url = url
+
+    @property
+    def some_fact(self):
+        fact_request = requests.get(self.url)
+        fact = fact_request.json()['text']
+        return fact
 
 
+def send_notifications():
+    send_fact = Fact()
+    send_text = send_fact.some_fact
+    for user in userlist:
+        bot.send_message(user,
+
+                f"Hello, my dear friend!\n"
+                f"today is {datetime.datetime.now()}\n"
+                f"Today some interesting fact about cats is:\n"
+                f"{send_text}"
+
+                        )
+    time.sleep(60)
 
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-	bot.reply_to(message, "i don't understand, sorry")
+class Repeated_func:
 
+    def __init__(self, func):
+        self.func = func
+        self.isCancel = False
+
+    def _run(self):
+        while not self.isCancel:
+            self.func()
+
+    def run(self):
+        self.isCancel = False
+        thread = Thread(target=Repeated_func._run, args={self})
+        thread.start()
+
+    def stop(self):
+        self.isCancel = True
+
+
+repeatable_func = Repeated_func(send_notifications)
+
+
+@bot.message_handler(content_types=['text'])
+def get_send_message(message):
+    if message.text.lower() == 'enable notifications':
+        userlist.append(message.from_user.id)
+
+    elif message.text.lower() == 'disable notifications':
+        userlist.remove(message.from_user.id)
+
+
+repeatable_func.run()
 bot.polling()
